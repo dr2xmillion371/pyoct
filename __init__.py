dcb8331a28a138eb3e1815d1f536684cc3d9464d
dr2xmillion371 <119767559+dr2xmillion371@users.noreply.github.com>
2023-04-19 18:26:18 -0500

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from pyoct import jkam
+
+
+jkam(0,10001,0.01)
---
+++ pyoct.py
@@ -0,0 +1,24 @@
+# start
+
+
+"""
+
+
+Copyright (C) The Jkam project -- public domain, Twirlybird software.
+
+
+"""
+
+
+
+import time
+
+
+def jkam(start,end,sp):
+	for i in range(start,end):
+		print(oct(i))
+		time.sleep(sp)
+
+
+
+# end
